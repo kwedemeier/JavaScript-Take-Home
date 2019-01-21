3dd158aa1922d7ba7fdec7698108fa4ce28bfcc8
kwedemeier <46731343+kwedemeier@users.noreply.github.com>
2019-01-20 23:29:25 -0600

    Create jokes api response

---
+++ allJokes.js
@@ -0,0 +1,64 @@
+// API Response of Jokes
+
+var allJokes = [  
+   {  
+	  "id":52,
+	  "type":"general",
+	  "setup":"Why did the invisible man turn down the job offer?",
+	  "punchline":"He couldn't see himself doing it"
+   },
+   {  
+	  "id":11,
+	  "type":"general",
+	  "setup":"What's the best time to go to the dentist?",
+	  "punchline":"Tooth hurty."
+   },
+   {  
+	  "id":55,
+	  "type":"general",
+	  "setup":"Why is peter pan always flying?",
+	  "punchline":"Because he neverlands"
+   },
+   {  
+	  "id":53,
+	  "type":"general",
+	  "setup":"How do you make holy water?",
+	  "punchline":"You boil the hell out of it"
+   },
+   {  
+	  "id":30,
+	  "type":"general",
+	  "setup":"What did the duck say when he bought lipstick?",
+	  "punchline":"Put it on my bill"
+   },
+   {  
+	  "id":42,
+	  "type":"general",
+	  "setup":"If you see a robbery at an Apple Store...",
+	  "punchline":"Does that make you an iWitness?"
+   },
+   {  
+	  "id":44,
+	  "type":"general",
+	  "setup":"Why did the Clydesdale give the pony a glass of water?",
+	  "punchline":"Because he was a little horse"
+   },
+   {  
+	  "id":46,
+	  "type":"general",
+	  "setup":"Finally realized why my plant sits around doing nothing all day...",
+	  "punchline":"He loves his pot."
+   },
+   {  
+	  "id":40,
+	  "type":"general",
+	  "setup":"Want to hear a joke about a peice of paper?",
+	  "punchline":"Never mind...it's tearable"
+   },
+   {  
+	  "id":13,
+	  "type":"knock-knock",
+	  "setup":"Knock knock. \n Who's there? \n Cows go. \n Cows go who?",
+	  "punchline":"No, cows go moo."
+   }
+];
